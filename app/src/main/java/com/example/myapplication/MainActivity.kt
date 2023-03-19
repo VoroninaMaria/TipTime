2e54768c7be50c2ca281429eeda13745cb783b92
VoroninaMaria <108742947+VoroninaMaria@users.noreply.github.com>
2023-03-19 15:56:53 +0200

    View binding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myButton: Button = binding.calculateButton
+        myButton.text = "A button"
     }
+    lateinit var binding: ActivityMainBinding
+
+
 }
+
